db49d77521e0a68a136a93fd14194fb0520dc3ac
Sebastian <winigossos@gmail.com>
2025-10-20 11:04:26 -0300

    finish

---
+++ src/ej2/Bateria.java
@@ -0,0 +1,19 @@
+package ej2;
+
+public class Bateria {
+    private String modelo;
+    private String capacidad;
+
+    public Bateria(String modelo, String capacidad) {
+        this.modelo = modelo;
+        this.capacidad = capacidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Bateria{" + "modelo=" + modelo + ", capacidad=" + capacidad + '}';
+    }
+    
+    
+    
+}
